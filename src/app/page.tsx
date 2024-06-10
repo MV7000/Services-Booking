ee9165cb159bb4b829a2b4324dc4773493307a4b
Volodymyr Magera <mwprestige@ukr.net>
2024-06-11 01:32:23 +0300

    services added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+'use client'
 import PreloaderHome from '@/components/PreloaderHome/PreloaderHome';
 import HomeElement from '../components/Home/HomeElement';
 
 export default function Home() {
+
   return (
     <main className='bg-[linear-gradient(90deg,_rgba(0,109,125,1)_0%,_rgba(201,241,247,1)_35%,_rgba(76,164,177,1)_100%)]'>
-      <PreloaderHome />
+     {!sessionStorage.getItem('preloader')&&<PreloaderHome />}
       <HomeElement />
     </main>
   );
